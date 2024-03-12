ba083fde985672bd89bfdbdb0f3e5219c61161e7
RadhikaBhattShringirishi <129614192+radhikabhattshringirishi@users.noreply.github.com>
2024-03-12 10:26:44 +0300

    DeliveryBoy Location Publish

---
+++ src/main/java/com/deliveryBoy/ZomatoDeliveryBoy/ZomatoDeliveryBoyApplication.java
@@ -0,0 +1,13 @@
+package com.deliveryBoy.ZomatoDeliveryBoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZomatoDeliveryBoyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZomatoDeliveryBoyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deliveryBoy/ZomatoDeliveryBoy/ZomatoDeliveryBoyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deliveryBoy.ZomatoDeliveryBoy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZomatoDeliveryBoyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
